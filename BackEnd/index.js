ea100e931eff17caa55035df549a36619a7a125a
M. Azhar Khan <ak3135311531@gmail.com>
2024-01-25 17:22:55 +0500

    Update index.js

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -31,7 +31,13 @@ const app = express();
 
 app.use(express.json())
 
-app.use(cors())
+app.use(cors(
+    {
+        origin : ["https://quizly-nine.vercel.app"],
+        methods : ["POST", "GET"],
+        credentials : true
+    }
+))
 
 app.get('/', (req, res)=>{
     console.log(req)
