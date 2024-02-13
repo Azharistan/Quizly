f4e06fdd5545407d9f2326d39e272505f1b84eed
M. Azhar Khan <ak3135311531@gmail.com>
2024-02-13 16:43:13 +0500

    changes in api key

--- BackEnd/config.js
+++ BackEnd/config.js
@@ -1,7 +1,7 @@
 export const PORT = 5000;
 
 //for local server using MONGOD~MONGOSH
-// export const MongoDBURL = 'mongodb://127.0.0.1:27017/Quizly';
+export const MongoDBURL = 'mongodb://127.0.0.1:27017/Quizly';
 
 //for cloud server Quizly~tut
-export const MongoDBURL = 'mongodb+srv://Azharistan:Azharistan@tut.uiiulfv.mongodb.net/?retryWrites=true&w=majority'
+// export const MongoDBURL = 'mongodb+srv://Azharistan:Azharistan@tut.uiiulfv.mongodb.net/?retryWrites=true&w=majority'
